4202860ee74dc53f687131aacf74b68f74353cc5
gabriel gómez de la torre <gabrielgomez@indigital.pe>
2022-08-25 01:52:49 -0500

    adding bb/100 graph

--- simulate.py
+++ simulate.py
@@ -1,4 +1,6 @@
 from pypokerengine.api.game import start_poker, setup_config
+import itertools
+import matplotlib.pyplot as plt
 
 from callbot import CallBot
 from databloggerbot import DataBloggerBot
@@ -9,14 +11,26 @@ if __name__ == '__main__':
 
     # The stack log contains the stacks of the Data Blogger bot after each game (the initial stack is 100)
     stack_log = []
-    for round in range(1):
-        p1, p2, p3= blogger_bot, CallBot(), CallBot()
+    stack_values = []
+    stack_values_2 = []
+    for round in range(50):
+        p1, p2= blogger_bot, CallBot()
 
-        config = setup_config(max_round=7, initial_stack=100, small_blind_amount=5)
+        config = setup_config(max_round=7, initial_stack=500, small_blind_amount=5)
         config.register_player(name="p1", algorithm=p1)
         config.register_player(name="p2", algorithm=p2)
-        config.register_player(name="p3", algorithm=p3)
         game_result = start_poker(config, verbose=1)
 
         stack_log.append([player['stack'] for player in game_result['players'] if player['uuid'] == blogger_bot.uuid])
+        stack_values.append((int(np.mean(stack_log))))
         print('Avg. stack:', '%d' % (int(np.mean(stack_log))))
+        print(stack_values)
+        print(stack_log)
+        #print(list(itertools.chain(*stack_log)))
+        
+    sum_list = itertools.accumulate(list(itertools.chain(*stack_log)))
+    for index,num in enumerate(sum_list):
+        print(num-(500*(index+1)))
+        stack_values_2.append(num-(500*(index+1)))
+    plt.plot(stack_values_2)
+    plt.show()
